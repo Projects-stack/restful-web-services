174333a4fae2ef57a449c8d98612befa23ff66e5
Sanket Mhetre <sanketmhetre66@gmail.com>
2023-09-03 15:40:01 +0530

    [feat]: added debug logging level and created hello-world-bean

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/bean/HelloWorldBean.java
@@ -0,0 +1,27 @@
+package com.in28minutes.rest.webservices.restfulwebservices.bean;
+
+public class HelloWorldBean
+{
+    private String message;
+
+    public HelloWorldBean(String message)
+    {
+        this.message = message;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "HelloWorldBean{" + "message='" + message + '\'' + '}';
+    }
+
+    public String getMessage()
+    {
+        return message;
+    }
+
+    public void setMessage(String message)
+    {
+        this.message = message;
+    }
+}
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -1,13 +1,40 @@
 package com.in28minutes.rest.webservices.restfulwebservices.controller;
 
+import com.in28minutes.rest.webservices.restfulwebservices.bean.HelloWorldBean;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController
 {
+
+    /*
+     * How are our requests handled?
+     * Auto Configuration (DispatcherServletAutoConfiguration)
+     *
+     * How does HelloWorldBean object gets converted into json?
+     * Auto Configuration (JacksonHttpMessageConvertersConfiguration)
+     *
+     * Who is configuring error mapping?
+     * Auto Configuration (ErrorMvcAutoConfiguration)
+     *
+     * How are all jars available?
+     * Spring Boot Starter Web Dependency consists of ->
+     *  1. spring-web mvc
+     *  2. spring-web
+     *  3. spring-boot-starter-tomcat
+     *  4. spring-boot-starter-json
+    */
+
+
     @GetMapping("/hello-world")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
     }
 }
