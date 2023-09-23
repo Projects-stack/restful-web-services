69470c9ae475fe7dfa9f26c44d29b984c7f2000e
Sanket Mhetre <sanketmhetre66@gmail.com>
2023-09-23 22:10:00 +0530

    [feat]: added sql data to h2-db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user_details (id, birth_date, name) VALUES (1001, '2000-01-01', 'Ranga');
+INSERT INTO user_details (id, birth_date, name) VALUES (1002, '2000-01-01', 'Ravi');
+INSERT INTO user_details (id, birth_date, name) VALUES (1003, '2000-01-01', 'Satish');
